0ab8622c62f334d832e9c3272bb3b1819038870a
Edison-Enrique Crespo-Vasquez <edison.crespo.ext@inetum.com>
2025-03-28 10:08:36 +0100

    Ejercicio obtenecion de datos con axios y react

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,29 +1,26 @@
 import "./assets/App.css";
 import React from "react";
-import { useState } from "react";
-import Note from "./pt2/components/Note";
-import PersonaAdd from "./pt2/components/PersonaAdd";
-import FilterPersona from "./pt2/components/FilterPersona";
-import PersonaAll from "./pt2/components/PersonaAll";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const App = () => {
-  const [personas, setPersonas] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-    { name: "Edison Crespo", number: "39-23-6423122", id: 5 },
-    { name: "Alex Garcia", number: "39-23-6423122", id: 6 },
-    { name: "Maria Love", number: "39-23-6423122", id: 7 },
-    { name: "Daniel Crespo", number: "39-23-6423122", id: 8 },
-  ]);
+  const [personas, setPersonas] = useState([]);
 
+  const responseNote = () => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersonas(response.data);
+    });
+  };
+  useEffect(responseNote, []);
   return (
     <div>
       <h1>Agencia telefonica</h1>
-      <PersonaAdd personas={personas} setPersonas={setPersonas} />
-      <FilterPersona personas={personas} />
-      <PersonaAll personas={personas} />
+      <p>N* Contactos: {personas.length}</p>
+      {personas.map((persona, i) => (
+        <p key={persona.id}>
+          N* {i} : {persona.name} Numero telefono: {persona.number}
+        </p>
+      ))}
     </div>
   );
 };
